bd8d448e9fdd80c4e6e1dfaf5d8cdc268bc1b1b6
Vinay Garg <xvinay28x@gmail.com>
2021-01-06 17:53:02 +0530

    no change

--- convert.c
+++ convert.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main(){
     char lower;
-    printf("Write lower case Latter  : ");
+    printf("Write lower case Letter  : ");
     scanf("%c",&lower);
     lower = lower - 32;
-    printf("Upper case Latter  : %c",lower);
+    printf("Upper case Letter  : %c",lower);
     return 0;
 }
